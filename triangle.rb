6934abc5cbc486727f56d81812d01e0f5e3b5fcc
Aayush sharma <greatash90@gmail.com>
2017-02-26 15:18:35 +0530

    Triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+   if  (a == b) && (a == c)
+     :equilateral
+   elsif (a.eql?(b)) || (b.eql?(c)) || (c.eql?(a))
+     :isosceles
+   else
+     :scalene
+   end
+ # puts :scalene if a.eql?(b) && b.eql?(c) && c.eql?(a)
+ # puts :isosceles if a.eql?(b) || b.eql?(c) || c.eql?(a)
 end
 
 # Error class used in part 2.  No need to change this code.
